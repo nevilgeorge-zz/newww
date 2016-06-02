4540f638ab17f3788941c5fff95b858e2727ea67
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-06-02 14:39:40 -0500

    adding animate.css

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,10 +6,9 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer">
-        <ul className="animated fadeUp nav-bar">
+        <ul className="animated fadeInUp nav-bar">
           <li><a href="/">home</a></li>
           <li><a href="/">experience</a></li>
-          <li><a href="/">readings</a></li>
         </ul>
       </footer>
     );
--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,8 @@
 
 import './styles/main.scss';
 import './styles/font-awesome.css';
-import './styles/ropa-sans.css'
+import './styles/ropa-sans.css';
+import './styles/animate.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
